5708414d2f43da8a7fe2169f8b2072bd4f2c5751
Mudasir Pandith <78873815+mudasirpandith@users.noreply.github.com>
2023-03-06 20:49:13 +0530

    test

--- config/database.js
+++ config/database.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose");
 const connectDataBase = () => {
   mongoose
     .connect("mongodb+srv://mudasir:Pass123@cluster0.tet8x.mongodb.net/ECOMDB?retryWrites=true&w=majority", {
-
       // .connect("mongodb://localhost:27017/NewDb", {// useNewUrlParser: true,
       // useUnifiedTopolgy: true,
       // useCreateIndex: true,
+      keepAlive: 9000*1000000000000*100000
     })
     .then((con) => {
       console.log(
@@ -16,3 +16,4 @@ const connectDataBase = () => {
 };
 
 module.exports = connectDataBase;
+s
