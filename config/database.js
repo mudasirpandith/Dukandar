16c44d89d1edeb9ddc1b5495822a2e45256acb01
Mudasir Pandith <78873815+mudasirpandith@users.noreply.github.com>
2023-03-15 19:47:22 +0530

    added pooling

--- config/database.js
+++ config/database.js
@@ -1,18 +1,28 @@
 const mongoose = require("mongoose");
+const redis = require('redis');
+// const { promisify } = require('util');
 // mongodb + srv://mudasir:Pass123@cluster0.tet8x.mongodb.net/EMARTDB?retryWrites=true&w=majority
 const connectDataBase = () => {
   mongoose
     .connect("mongodb+srv://mudasir:Pass123@cluster0.tet8x.mongodb.net/ECOMDB?retryWrites=true&w=majority", {
-
-      // .connect("mongodb://localhost:27017/NewDb", {// useNewUrlParser: true,
-      // useUnifiedTopolgy: true,
-      // useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     })
     .then((con) => {
       console.log(
         `MongoDB Database connected with host : ${con.connection.host}`
       );
     });
+  mongoose.set('debug', true);
+  mongoose.connect('mongodb+srv://mudasir:Pass123@cluster0.tet8x.mongodb.net/ECOMDB?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    poolSize: 10,
+    bufferMaxEntries: 0,
+  });
+  // const redisClient = redis.createClient();
+  // const getAsync = promisify(redisClient.get).bind(redisClient);
+  // const setAsync = promisify(redisClient.set).bind(redisClient);
 };
 
 module.exports = connectDataBase;
